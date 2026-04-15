7070968a80eaf7d1560e7e721dee8d4c9d53673c
Egan <144761969+Egan-1511@users.noreply.github.com>
2026-04-15 12:20:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "AI SmartLab Backend Running 🚀"
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    file = request.files["file"]
+    df = pd.read_csv(file)
+
+    numeric = df.select_dtypes(include='number')
+
+    result = {
+        "rows": df.shape[0],
+        "columns": df.shape[1],
+        "columns_list": df.columns.tolist(),
+        "missing": df.isnull().sum().to_dict(),
+        "correlation": numeric.corr().fillna(0).to_dict()
+    }
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run()
